44cce2c80ef25d181f0808864054da26de45357e
Radu Chindris <radu.chindris@endava.com>
2025-01-30 10:43:40 +0200

    Elements_Buttons

--- src/test/java/Tests/Elements_CheckBox.java
+++ src/test/java/Tests/Elements_CheckBox.java
@@ -11,35 +11,35 @@ public class Elements_CheckBox {
     public WebDriver driver;
     @Test
 
-    public void checkBox(){
-        driver=new EdgeDriver();
+    public void checkBox() {
+        driver = new EdgeDriver();
         driver.get("https://demoqa.com/text-box");
         driver.manage().window().maximize();
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,400)");
 
-        WebElement textBox=driver.findElement(By.xpath("//span[text()='Check Box']"));
+        WebElement textBox = driver.findElement(By.xpath("//span[text()='Check Box']"));
         textBox.click();
 
         //button[@class='rct-collapse rct-collapse-btn']
-        WebElement dropDownListHome=driver.findElement(By.xpath("//*[@id='tree-node']/ol/li/span/button"));
+        WebElement dropDownListHome = driver.findElement(By.xpath("//*[@id='tree-node']/ol/li/span/button"));
         dropDownListHome.click();
 
-        WebElement dropDownListDesktop=driver.findElement(By.xpath("//*[@id='tree-node']/ol/li/ol/li[1]/span/button"));
+        WebElement dropDownListDesktop = driver.findElement(By.xpath("//*[@id='tree-node']/ol/li/ol/li[1]/span/button"));
         dropDownListDesktop.click();
-        WebElement selectNotes=driver.findElement(By.xpath("//span[text()='Notes']"));
+        WebElement selectNotes = driver.findElement(By.xpath("//span[text()='Notes']"));
         selectNotes.click();
 
-        WebElement dropDownListDocuments=driver.findElement(By.xpath("//*[@id='tree-node']/ol/li/ol/li[2]/span/button"));
+        WebElement dropDownListDocuments = driver.findElement(By.xpath("//*[@id='tree-node']/ol/li/ol/li[2]/span/button"));
         dropDownListDocuments.click();
-        WebElement selectOffice=driver.findElement(By.xpath("//span[text()='Office']"));
+        WebElement selectOffice = driver.findElement(By.xpath("//span[text()='Office']"));
         selectOffice.click();
 
-        WebElement dropDownListDownlods=driver.findElement(By.xpath("//*[@id='tree-node']/ol/li/ol/li[3]/span/button"));
+        WebElement dropDownListDownlods = driver.findElement(By.xpath("//*[@id='tree-node']/ol/li/ol/li[3]/span/button"));
         dropDownListDownlods.click();
-        WebElement selectWordFile=driver.findElement(By.xpath("//span[text()='Word File.doc']"));
+        WebElement selectWordFile = driver.findElement(By.xpath("//span[text()='Word File.doc']"));
         selectWordFile.click();
-        WebElement selectExcelFile=driver.findElement(By.xpath("//span[text()='Excel File.doc']"));
+        WebElement selectExcelFile = driver.findElement(By.xpath("//span[text()='Excel File.doc']"));
         selectExcelFile.click();
 
     }
